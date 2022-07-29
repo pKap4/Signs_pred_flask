108d70ec864162deb4839b5193053257a68bfff5
pKap4 <91935893+pKap4@users.noreply.github.com>
2022-07-29 13:10:30 +0530

    Updated

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from keras.preprocessing import image
 from matplotlib.pyplot import imshow
 import numpy as np
 import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = "./static/"
@@ -12,7 +13,6 @@ dic = {0:'A', 1:'B', 2:'C', 3:'D', 4:'E', 5:'F', 6:'G', 7:'H', 8:'I', 9:'J', 10:
 
 model = load_model('signs_resnet.h5', compile=False)
 
-print(model)
 def predict_label(img_path):
     i = image.load_img(img_path, target_size = (64, 64))
     i = image.img_to_array(i)
@@ -23,23 +23,29 @@ def predict_label(img_path):
     p = np.argmax(p)
     return dic[p]
 
-#routes
-@app.route('/', methods = ['GET', 'POST'])
-def base():
-    if request.method == 'POST':
-        try:
-            img = request.files['my_image']
-            img_path = './static/'+ img.filename
-            path = os.path.join(app.config['UPLOAD_FOLDER'], img.filename)
-            img.save(path)
-        except Exception as e:
-            print(e)
-        
-        #p = predict_label(img_path)
-        #return render_template('index.html', prediction = p)
-        return "Successful"
-    return render_template('index.html')
+def get_file_path_and_save(request):
+    # Get the file from post request
+    f = request.files['file']
 
+    # Save the file to ./static
+    basepath = os.path.dirname(__file__)
+    file_path = os.path.join(
+        basepath, 'static', secure_filename(f.filename))
+    f.save(file_path)
+    return file_path
+
+#routes
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    request_type_str = request.method
+    if request_type_str == 'GET':
+        # Main page
+        return 
+    else:
+        file_path = get_file_path_and_save(request)
+        prediction = predict_label(file_path)
+        return render_template('index.html', prediction = prediction)
+        
 if __name__ == "__main__":
     app.run()
 
