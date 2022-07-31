0853b6e78a9ca61eeb6fea05dab52b5f5eff3999
pKap4 <91935893+pKap4@users.noreply.github.com>
2022-07-31 08:06:04 +0530

    char dic deleted, nums 0-5 output

--- app.py
+++ app.py
@@ -13,8 +13,6 @@ UPLOAD_FOLDER = "/Users/Parth Kapadia/Desktop/Signs Tf FLASK/main/Signs_pred_fla
 model = load_model('signs_resnet.h5', compile=False)
 @app.route('/', methods = ['GET', 'POST'])
 def base():
-    dic = {0:'A', 1:'B', 2:'C', 3:'D', 4:'E', 5:'F', 6:'G', 7:'H', 8:'I', 9:'J', 10:'K', 11:'L', 12:'M', 13:'N', 14:'O', 15:'P', 16:'Q', 17:'R', 18:'S', 19:'T', 20:'U', 21:'V', 22:'W', 23:'X', 24:'Y', 25:'Z'}
-
     if request.method == 'POST':
         image_file = request.files["image"]
         if image_file:
@@ -29,7 +27,6 @@ def base():
             p = model.predict(i)
             p = np.argmax(p)
             print(p)
-            p = dic[p]
             
         return render_template("index.html",prediction = p)
     return render_template("index.html", prediction='-')
